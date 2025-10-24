28e57b3e06eedbb72eb3e5c8d2320458a385bfdd
Lucas Rocha <lucasrocha@ucl.br>
2025-10-24 20:20:55 -0300

    add

--- src/unit_tests.py
+++ src/unit_tests.py
@@ -61,3 +61,5 @@ test_confirmarPagamento_success()
 test_confirmarPagamento_fail()
 test_submeterPagamento_success()
 test_submeterPagamento_multiple()
+
+print("Todos os testes passaram com sucesso!")
